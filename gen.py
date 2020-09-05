d96f8dc9216e3b169c65985bfa594505d28152c2
Tic <44981148+yougotsyskeyed@users.noreply.github.com>
2020-09-05 09:43:26 +0100

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,13 @@
+import random
+
+def randomStringDigits(stringLength=13) :
+    # Generate a random string of letters and digits
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
+providers = ["@gmail.com", "@googlemail.com", "@gmx.de","@outlook.com","@outlook.fr","@yandex.ru","@mail.com","@yahoo.co.uk","@protonmail.ch"]
+
+
+rngemail = randomStringDigits(10)
+
+print((rngemail) + random.choice(providers))
